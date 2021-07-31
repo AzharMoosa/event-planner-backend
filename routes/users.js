f9860daf32e7df0c8ae4b4a97feb99cc87f608e6
AzharMoosa <azhar.moosa8@gmail.com>
2021-07-31 15:28:09 +0100

    Connected User Route To Controller

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
 import express from "express";
 const router = express.Router();
-import { loginUser, registerUser } from "../controllers/userController.js";
+import {
+  loginUser,
+  registerUser,
+  getUsers,
+  updateUser,
+  deleteUser,
+  getUserByID,
+} from "../controllers/userController.js";
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(getUsers);
 router.route("/login").post(loginUser);
+router.route("/login").post(loginUser);
+router.route("/:id").get(getUserByID).put(updateUser).delete(deleteUser);
 
 export default router;
