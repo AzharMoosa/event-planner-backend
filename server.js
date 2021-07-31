324d091efc0411962df193281786d3d7a51aaaab
AzharMoosa <azhar.moosa8@gmail.com>
2021-07-31 15:33:56 +0100

    Error Middleware

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ import express from "express";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import userRoute from "./routes/users.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 const app = express();
+
+app.use(express.json());
 
 // Connect To MongoDB
 connectDB();
 
 app.use("/api/users", userRoute);
+
+// Error Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
